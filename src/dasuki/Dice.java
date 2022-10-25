104a6f108b2de96d560c6cb4258ebed14c4e3d46
Emir-D <emir@degirmenci.dk>
2022-10-25 11:05:38 +0200

    Lavet tærning færdig næsten, mangler enkelte ting.

--- src/dasuki/Dice.java
+++ src/dasuki/Dice.java
@@ -6,27 +6,15 @@ public class Dice {
     public int Dice1;
     public int Dice2;
 
-    public Dice(int Dice1, int Dice2) {
-        this.Dice1 = Dice1;
-        this.Dice2 = Dice2;
-        roll();
+    public int getSum() {
+        return Dice1 + Dice2;
     }
+
     public int roll() {
         Random ran = new Random();
         Dice1 = ran.nextInt(6)+1;
         Dice2 = ran.nextInt(6)+1;
         return getSum();
-    }
-    public int getSum() {
-        return Dice1 + Dice2;
-    }
-
-    public int getDice1() {
-        return Dice1;
-    }
-
-    public int getDice2() {
-        return Dice2;
     }
 
 }
